916a5604e942f24748a91301ffe6448b8ee82663
Amitesh92 <amiteshmaurya01@gmail.com>
2020-02-18 18:38:33 +0530

    Update webpack.config.js
    modified webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+
 
 module.exports = {
   mode: 'development',
@@ -10,60 +13,61 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.(sa|sc|c)ss$/i,
-            use: [
-                 MiniCssExtractPlugin.loader,
-                'css-loader', // translates CSS into CommonJs
-                'sass-loader' // compiles Sass to CSS
-            ]
-        },
-        {
-            test: /\.m?js$/i,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            } 
-        },
-        {
-            test: /\.(png|jpe?g|gif)$/i,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                    outputPath: 'images'
-                  }
+      {
+          test: /\.(sa|sc|c)ss$/i,
+          use: [
+               MiniCssExtractPlugin.loader,
+              'css-loader', // translates CSS into CommonJs
+              'sass-loader' // compiles Sass to CSS
+          ]
+      },
+      {
+          test: /\.m?js$/i,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['@babel/preset-env']
               }
-            ],
-        },
-        {
-            // Apply rule for fonts files
-            test: /\.(woff|woff2|ttf|otf|eot)$/,
-            use: [
-                   {
-                     // Using file-loader too
-                     loader: "file-loader",
-                     options: {
-                       outputPath: 'fonts'
-                     }
+          } 
+      },
+      {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                  outputPath: 'images'
+                }
+            }
+          ],
+      },
+      {
+          // Apply rule for fonts files
+          test: /\.(woff|woff2|ttf|otf|eot)$/,
+          use: [
+                 {
+                   // Using file-loader too
+                   loader: "file-loader",
+                   options: {
+                     outputPath: 'fonts'
                    }
-                 ]
-          }
+                 }
+               ]
+        }
 
-    ]
+  ]
   },
   plugins: [
-      new MiniCssExtractPlugin({
-        filename: "bundle.css"
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    }),
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
       })
   ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
+
   watch: true
 };
